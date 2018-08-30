065586cd5c90597ce43af55de266af37377962ba
leonardophildeg <leonardo.phildeg@bridge.ufsc.br>
2018-08-29 21:51:48 -0300

    translate

--- src/backtracking/NRainhasBacktracking.java
+++ src/backtracking/NRainhasBacktracking.java
@@ -1,12 +1,25 @@
 package backtracking;
 
+import java.util.Scanner;
+
 public class NRainhasBacktracking {
 
 	public static void main(String[] args) {
-		placeQueens(16); // No exemplo, um tabuleiro de 4x4
+		long tempoInicial = System.currentTimeMillis();
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("---------- Insira o tamanho da tabela ----------");
+		int tamanhoTabuleiro = scanner.nextInt();
+		System.out.println("");
+
+		inserirRainha(tamanhoTabuleiro); // No exemplo, um tabuleiro de 4x4
+
+		long tempoFinal = System.currentTimeMillis() - tempoInicial;
+		System.out.println("O algoritmo foi executado em " + tempoFinal / 1000 + " segundos");
+
 	}
 
-	private static void placeQueens(int gridSize) {
+	private static void inserirRainha(int tamanhoTabuleiro) {
 
 		// Se a tabuleiro é 1x1 ou 2x2 ou 3x3, então não temos solução possível.
 		// No tabuleiro de 1x1 ou 2x2, rainhas inseridas na primeira linha, em qualquer
@@ -16,55 +29,55 @@ public class NRainhasBacktracking {
 		// as combinações
 		// de posições atacará a rainha colocada em qualquer posição na linha 3
 
-		if (gridSize < 4) {
-			System.out.println("No Solution available");
+		if (tamanhoTabuleiro < 4) {
+			System.out.println("Sem solução para tabuleiros menores que 4x4.");
 		} else {
-			int[][] board = new int[gridSize][gridSize];
-			placeAllQueens(board, 0);
-			printBoard(board);
+			int[][] tabuleiro = new int[tamanhoTabuleiro][tamanhoTabuleiro];
+			inserirTodasRainhas(tabuleiro, 0);
+			imprimirTabuleiro(tabuleiro);
 		}
 	}
 
-	private static boolean placeAllQueens(int board[][], int row) {
-		if (row >= board.length) {
+	private static boolean inserirTodasRainhas(int tabuleiro[][], int linha) {
+		if (linha >= tabuleiro.length) {
 			return true;
 		}
 
-		boolean isAllQueensPlaced = false;
-		for (int j = 0; j < board.length; j++) {
+		boolean todasRainhasInseridas = false;
+		for (int j = 0; j < tabuleiro.length; j++) {
 
-			if (isSafe(board, row, j)) {
-				board[row][j] = 1;
-				isAllQueensPlaced = placeAllQueens(board, row + 1);
+			if (ehSeguro(tabuleiro, linha, j)) {
+				tabuleiro[linha][j] = 1;
+				todasRainhasInseridas = inserirTodasRainhas(tabuleiro, linha + 1);
 			}
-			if (isAllQueensPlaced) {
+			if (todasRainhasInseridas) {
 				break;
 			} else {
-				board[row][j] = 0;
+				tabuleiro[linha][j] = 0;
 			}
 		}
-		return isAllQueensPlaced;
+		return todasRainhasInseridas;
 	}
 
-	private static boolean isSafe(int board[][], int row, int col) {
+	private static boolean ehSeguro(int tabuleiro[][], int linha, int coluna) {
 
-		// Check Left Upper Diagonal
-		for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-			if (board[i][j] == 1) {
+		// Faz a checagem na diagonal esquerda
+		for (int i = linha - 1, j = coluna - 1; i >= 0 && j >= 0; i--, j--) {
+			if (tabuleiro[i][j] == 1) {
 				return false;
 			}
 		}
 
-		// Check Right Upper Diagonal
-		for (int i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
-			if (board[i][j] == 1) {
+		// Faz a checagem na diagonal direita
+		for (int i = linha - 1, j = coluna + 1; i >= 0 && j < tabuleiro.length; i--, j++) {
+			if (tabuleiro[i][j] == 1) {
 				return false;
 			}
 		}
 
-		// Check in same Column
-		for (int i = row - 1; i >= 0; i--) {
-			if (board[i][col] == 1) {
+		// Faz a checagem na mesma coluna
+		for (int i = linha - 1; i >= 0; i--) {
+			if (tabuleiro[i][coluna] == 1) {
 				return false;
 			}
 		}
@@ -72,10 +85,10 @@ public class NRainhasBacktracking {
 		return true;
 	}
 
-	private static void printBoard(int[][] board) {
-		for (int row = 0; row < board.length; row++) {
-			for (int col = 0; col < board.length; col++) {
-				if (board[row][col] == 1) {
+	private static void imprimirTabuleiro(int[][] tabuleiro) {
+		for (int linha = 0; linha < tabuleiro.length; linha++) {
+			for (int col = 0; col < tabuleiro.length; col++) {
+				if (tabuleiro[linha][col] == 1) {
 					System.out.print("X ");
 				} else {
 					System.out.print("_ ");
